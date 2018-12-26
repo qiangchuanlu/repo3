a545a71d2bd65aa4081141744a5847387ef47b0b
luchuanqiang <843152350@qq.com>
2018-12-26 20:53:24 +0800

    将java工程提交到版本库

---
+++ day01_mybatis/src/test/java/mytest/MybatisTest.java
@@ -0,0 +1,39 @@
+package mytest;
+
+
+import dao.UserDao;
+import domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/*
+MybatisTest 入门案列
+ */
+public class MybatisTest {
+    public static void main(String[] args) throws Exception {
+
+        //1.读取配置文件
+        InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //2.创建SqlSessionFactory工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(is);
+        //3.使用工厂生产对象
+        SqlSession session = factory.openSession();
+        //4.使用
+        UserDao userDao = session.getMapper(UserDao.class);
+        //5.使用代理对象执行方法
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+        //6.释放资源
+        session.close();
+        is.close();
+    }
+}
